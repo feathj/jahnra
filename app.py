c4bb4785d074787f1b00aa271898c5a16279f9d8
Jonathan Featherstone <jfeatherstone@instructure.com>
2017-02-12 01:05:01 -0700

    Add pipeline and test page

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request, response, current_app
+from flask import Flask, render_template, request, Response, current_app
+from jahnra import Jahnra
+from json import dumps
 
 
 app = Flask(__name__)
-# with app.app_context():
-    # current_app.jahnra = Jahnra()
+with app.app_context():
+    current_app.jahnra = Jahnra('./db.csv', False)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# @app.route('/check_user', methods=['POST'])
-# def check_user():
-    # img_buffer = request.files['webcam']
-    # res = {}
-    # detected_faces = app.recognizer.detect_faces(img_buffer)
-    # res['detected_faces'] = detected_faces
-    # return Response(json.dumps(res),  mimetype='application/json')
+@app.route('/predict_genre', methods=['POST'])
+def guess_genre():
+    lyrics = request.json['lyrics']
+    prediction = app.jahnra.predict('ri', lyrics)
+
+    res = {}
+    res['prediction'] = prediction
+    # TODO other stuffs about prediction here
+    return Response(dumps(res), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(
--- jahnra.py
+++ jahnra.py
@@ -7,47 +7,53 @@ from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
+from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.linear_model import Perceptron
-# from sklearn.linear_model import RidgeClassifier
-# from sklearn.linear_model import PassiveAggressiveClassifier
-# from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import SGDClassifier
 
 
 class Jahnra:
+    DOCUMENT_THRESHOLD = 1000
 
-    def __init__(self, db_path):
+    def __pipeline(self, classifier):
+        return Pipeline([
+            ('count_vectorizer', CountVectorizer(ngram_range=(1,  2))),
+            ('tfidf_transformer', TfidfTransformer()),
+            ('classifier', classifier)])
+
+    def __init__(self, db_path, test=False):
         # init
-        self.classifier = MultinomialNB()
-        # self.classifier = RandomForestClassifier(n_estimators=100)
-        # self.classifier = Perceptron(n_iter=50)
-        # self.classifier = RidgeClassifier(tol=1e-2, solver="lsqr")
-        # self.classifier = PassiveAggressiveClassifier(n_iter=50)
-        # self.classifier = KNeighborsClassifier(n_neighbors=10)
-
-        self.count_vectorizer = CountVectorizer(
-                ngram_range=(1, 3),
-                analyzer="word",
-                tokenizer=None,
-                preprocessor=None,
-                stop_words=None,
-                max_features=5000)
+        self.classifiers = {}
+        self.classifiers['nb'] = self.__pipeline(MultinomialNB())
+        # self.classifiers['rf'] = self.__pipeline(RandomForestClassifier(n_estimators=100))
+        self.classifiers['pe'] = self.__pipeline(Perceptron(n_iter=50))
+        self.classifiers['ri'] = self.__pipeline(RidgeClassifier(tol=1e-2, solver="lsqr"))
+        self.classifiers['pg'] = self.__pipeline(PassiveAggressiveClassifier(n_iter=50))
+        self.classifiers['kn'] = self.__pipeline(KNeighborsClassifier(n_neighbors=10))
+        self.classifiers['sgd'] = self.__pipeline(SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42))
 
         self.tokenizer = RegexpTokenizer(r'\w+')
         self.stop = Set(stopwords.words('english'))
         self.stemmer = SnowballStemmer('english')
 
         # train
-        self.__train_classifier(db_path)
+        self.__train_classifiers(db_path, test)
 
     def __clean_text(self, text):
         tokens = self.tokenizer.tokenize(text)
-        tokens = [self.stemmer.stem(t.lower()) for t in tokens if not self.stemmer.stem(t.lower()) in self.stop]
+        tokens = [t.lower() for t in tokens]
+        tokens = [self.stemmer.stem(t) for t in tokens]
+        tokens = [t for t in tokens if t not in self.stop]
         return ' '.join(tokens)
 
-    def __train_classifier(self, db_path):
+    def __train_classifiers(self, db_path, test=False):
         # load
         print 'loading'
         data = {}
@@ -60,32 +66,47 @@ class Jahnra:
                     'label': row['genre'],
                     'text': self.__clean_text(row['lyrics'])
                     })
+            for genre, records in data.iteritems():
+                if len(records) >= Jahnra.DOCUMENT_THRESHOLD:
+                    print '{0}: {1}'.format(genre, len(records))
 
         # train
         print 'training'
         train_data = []
         test_data = []
         for label, records in data.iteritems():
-            shuffle(records)
-            split_index = int(round(len(records) * 0.7))
-            train_data.extend(records[:split_index])
-            test_data.extend(records[split_index:])
+            if len(records) < Jahnra.DOCUMENT_THRESHOLD:
+                continue
+            if test:
+                shuffle(records)
+                split_index = int(round(len(records) * 0.7))
+                train_data.extend(records[:split_index])
+                test_data.extend(records[split_index:])
+            else:
+                train_data.extend(records)
 
-        counts = self.count_vectorizer.fit_transform([d['text'] for d in train_data])
-        self.classifier.fit(counts, [d['label'] for d in train_data])
+        train_text = [d['text'] for d in train_data]
+        train_labels = [d['label'] for d in train_data]
 
-        # test
-        print 'testing'
-        num_correct = 0
-        for d in test_data:
-            result = self.predict(d['text'])
-            if result == d['label']:
-                num_correct += 1
+        test_text = [d['text'] for d in test_data]
+        test_labels = [d['label'] for d in test_data]
+        test_results = {}
 
-        print '{0}/{1}'.format(num_correct, len(test_data))
+        for classifier_label, classifier in self.classifiers.iteritems():
+            print classifier_label
+            classifier.fit(train_text, train_labels)
+            if test:
+                predictions = classifier.predict(test_text)
+                correct_predictions = 0
+                for index, val in enumerate(predictions):
+                    if val == test_labels[index]:
+                        correct_predictions += 1
+                test_results[classifier_label] = (float(correct_predictions) / float(len(test_data))) * 100.0 
+        if test:
+            print test_results
 
-    def predict(self, text):
-        result = self.classifier.predict(self.count_vectorizer.transform([self.__clean_text(text)]))
+    def predict(self, classifier, text):
+        result = self.classifiers[classifier].predict([self.__clean_text(text)])
         return result[0]
 
-x = Jahnra('./db.csv')
+# x = Jahnra('./db.csv', True)
